59448652299b7b87a4850290841c3a1497e2cb5b
jashimgit <jashim.ncn@gmail.com>
2022-10-10 23:19:04 +0600

    login page modified

--- pages/login.js
+++ pages/login.js
@@ -6,24 +6,27 @@ import Head from "next/head";
 
 export default function Login() {
     return (
-        <div className="contianer-fluid">
-            <div className="col-6 d-flex align-items-center justify-content-center mt-4 pt-4">
-                <div className="card">
-                    <div className="card-body">
-                        <h4 className="card-title text-center">Login</h4>
-                        <form>
-                            <div className="mb-3">
-                                <label className="form-label">Phone</label>
-                                <input type="text" className="form-control" name="phone" />
-                            </div>
-                            <div className="mb-3">
-                                <label className="form-label">Pin</label>
-                                <input type="password" class="form-control" name="pin" />
-                            </div>
-                            <div>
-                                <button type="submit" className="w-100 btn btn-primary">Login</button>
-                            </div>
-                        </form>
+        <div className="contianer">
+            <div className="row justify-content-center">
+                <div className="col-xxl-4 col-lg-5 mt-4 pt-5">
+                    <div className="card mt-4">
+                      
+                        <div className="card-body">
+                        <h4 className="card-title">Login</h4>
+                            <form>
+                                <div className="mb-3">
+                                    <label className="form-label">Phone</label>
+                                    <input type="text" className="form-control" name="phone" />
+                                </div>
+                                <div className="mb-3">
+                                    <label className="form-label">Pin</label>
+                                    <input type="password" class="form-control" name="pin" />
+                                </div>
+                                <div>
+                                    <button type="submit" className="w-100 btn btn-primary">Login</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -31,3 +34,5 @@ export default function Login() {
 
     )
 }
+
+
